8f2ab688cd0427d6afd1de4b32ee0fe2b4f55709
Hebi Nguyen <lucius2k3@gmail.com>
2025-09-17 14:17:40 +0700

    show data

--- FE/pages/3_Escalation.py
+++ FE/pages/3_Escalation.py
@@ -1,10 +1,23 @@
 import streamlit as st
+import sqlite3
+import pandas as pd
+from pathlib import Path
+
+DB_PATH = Path(r"E:\Python\Customer_Support_Agents_Backend\BE\log.db")
+
+def getEscalation():
+    conn = sqlite3.connect(DB_PATH)
+    query = "SELECT question FROM query_logs WHERE flagged=1"
+    df = pd.read_sql(query, conn)
+    conn.close()
+    return df
 
 st.title("🚨 Escalation / Alerts")
 
-st.subheader("Danh sách câu hỏi đã escalate")
-st.table({
-    "Câu hỏi": ["Khách hàng hỏi giá custom", "Tích hợp Salesforce"],
-    "Trạng thái": ["Đang xử lý", "Chờ phản hồi"],
-    "Chịu trách nhiệm": ["Anh A", "Chị B"]
-})
+df = getEscalation()
+
+if df.empty:
+    st.info("✅ Chưa có câu hỏi nào bị escalate.")
+else:
+    st.subheader("Danh sách câu hỏi đã escalate")
+    st.table(df)
